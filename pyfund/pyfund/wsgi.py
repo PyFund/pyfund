471c1ef0c89f75b4f040fda276941375fafa8a56
Shawn Lin <xiaoliang.lin@columbia.edu>
2020-07-06 19:46:49 -0400

    Setup frontend skeleton

--- pyfund/pyfund/wsgi.py
+++ pyfund/pyfund/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyfund.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyfund.settings")
 
 application = get_wsgi_application()
