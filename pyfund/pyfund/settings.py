cbe145bbb8e5d110bfbbd4ea364a309b2436223b
Shawn Lin <xiaoliang.lin@columbia.edu>
2020-07-06 23:12:00 -0400

    Add authentication api endpoint

--- pyfund/pyfund/settings.py
+++ pyfund/pyfund/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "knox",
     "frontend",
+    "accounts",
 ]
+
+REST_FRAMEWORK = {"DEFAULT_AUTHENTICATION_CLASSES": ("knox.auth.TokenAuthentication",)}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- pyfund/pyfund/urls.py
+++ pyfund/pyfund/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("frontend.urls")),
-    path("admin/", admin.site.urls),
+    path("", include("accounts.urls")),
 ]
